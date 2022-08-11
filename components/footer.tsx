fde2d8f2bdf4bc7fb4771f39f7e97001f38efa28
rkoleci <rkoleci14@gmail.com>
2022-08-11 20:50:29 +0200

    Footer fixes

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,4 @@
 import { Flex, HStack, Link, Text } from "@chakra-ui/react";
-
 import { Icon } from "@chakra-ui/react";
 import { AiFillLinkedin } from "react-icons/ai";
 import { FaGithubSquare, FaInstagramSquare } from "react-icons/fa";
@@ -15,22 +14,26 @@ export default function Footer() {
       justifyContent="space-between"
       alignItems="center"
       px="20"
-      py="2"
+      pt="2"
+      pb="3"
     >
-      <Text fontWeight="300">
-        {`© ${new Date().getFullYear()} Rei Koleci — `}
-        <Text display="inline-block" fontWeight="500">
-          @rei.koleci
+      <Flex alignItems="center">
+        <Text fontWeight="400" fontSize="14">
+          {`© ${new Date().getFullYear()} Rei Koleci`}
         </Text>
-      </Text>
+        <Text mx="1">—</Text>
+        <Text display="inline-block" fontWeight="500" color="gray.600">
+          {`@rei.koleci`}
+        </Text>
+      </Flex>
       <HStack spacing="8">
-        <Link href="https://www.google.com" isExternal>
+        <Link href="https://www.linkedin.com/in/rei-tryreact/" target="_blank">
           <Icon fontSize="25" color="gray.100" as={AiFillLinkedin} />
         </Link>
-        <Link href="https://www.google.com" isExternal>
+        <Link href="https://github.com/rkoleci" target="_blank">
           <Icon fontSize="25" color="gray.100" as={FaGithubSquare} />
         </Link>
-        <Link href="https://www.google.com" isExternal>
+        <Link href="https://www.instagram.com/rei.koleci" target="_blank">
           <Icon fontSize="25" color="gray.100" as={FaInstagramSquare} />
         </Link>
       </HStack>
