9dd1129e812c95cc22ff43666b42b7e576a3a413
Rei Koleci <rkoleci14@gmail.com>
2022-08-18 21:54:19 +0200

    Dark mode support (#34)

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
       bottom="0"
       left="0"
       width="100%"
-      background="gray.700"
+      background="footer-bg"
       justifyContent="space-between"
       alignItems="center"
       px={["2", '10', '14', "20"]}
@@ -27,19 +27,19 @@ export default function Footer() {
           {`© ${new Date().getFullYear()} Rei Koleci`}
         </Text>
         <Text mx="1">—</Text>
-        <Text display="inline-block" fontWeight="500" color="gray.600">
+        <Text display="inline-block" fontWeight="500" color="footer-text">
           {`@rei.koleci`}
         </Text>
       </Flex>
       <HStack spacing="8" justifyContent={["center", "flex-end"]}>
         <Link href="https://www.linkedin.com/in/rei-tryreact/" target="_blank">
-          <Icon fontSize="25" color="gray.100" as={AiFillLinkedin} />
+          <Icon fontSize="25" color="footer-icon" as={AiFillLinkedin} />
         </Link>
         <Link href="https://github.com/rkoleci" target="_blank">
-          <Icon fontSize="25" color="gray.100" as={FaGithubSquare} />
+          <Icon fontSize="25" color="footer-icon" as={FaGithubSquare} />
         </Link>
         <Link href="https://www.instagram.com/rei.koleci" target="_blank">
-          <Icon fontSize="25" color="gray.100" as={FaInstagramSquare} />
+          <Icon fontSize="25" color="footer-icon" as={FaInstagramSquare} />
         </Link>
       </HStack>
     </Box>
